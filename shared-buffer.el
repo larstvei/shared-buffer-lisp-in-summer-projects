fc73aec1d6679a08366075b96b4acb012ff9dd91
Lars Tveito <lars@localhost.localdomain>
2013-07-19 03:45:54 +0200

    New clients now receives the buffer content from an existing client. Separated initiating a new shared-buffer-session and connecting to an existing one. Fixed some bugs as well.

--- shared-buffer.el
+++ shared-buffer.el
@@ -4,13 +4,16 @@
 ;; option) any later version.
 
 ;; This program is distributed in the hope that it will be useful, but
-;; WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
-;; Public License for more details.
+;; WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANT
+;; ABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+;; License for more details.
 
 (require 's)
 
-(cl-defstruct sb-package start bytes text)
+(defstruct sb-package
+  "This struct defines the format for packages sent to and
+  received from the server."
+  (start 1) (bytes 0) (text "") (for-new-client nil))
 
 (defconst *sb-port* 3705
   "Shared-buffer uses port 3705.")
@@ -19,21 +22,44 @@
   "A buffer-local variable pointing to the server a shared buffer is
   connected to.")
 
-(defun sb-connect-to-server (host &optional buffer)
+(defvar *sb-key* ""
+  "A buffer-local string containing the key to the associated
+  shared-bufer-session.")
+
+(defun sb-connect-to-server (host buffer)
   "This function opens a connection to a shared-buffer server, by starting a
 shared-buffer client."
-  (interactive "sHost: ")
+  (switch-to-buffer buffer)
   (make-local-variable '*sb-server*)
-  (let ((key (read-from-minibuffer "key: ")))
-    (setq *sb-server*
-          (make-network-process
-           :name "sb-client" :buffer (or buffer (current-buffer))
-           :filter 'sb-client-filter :sentinel 'sb-client-sentinel
-           :family 'ipv4 :host host :service *sb-port*))
-    (process-send-string *sb-server* (concat key "\n"))
-    (add-hook 'after-change-functions 'sb-update nil 'local)))
+  (make-local-variable '*sb-key*)
+  (setq *sb-key* (read-from-minibuffer "key: "))
+  (setq *sb-server*
+        (make-network-process
+         :name "sb-client" :buffer buffer
+         :filter 'sb-client-filter :sentinel 'sb-client-sentinel
+         :family 'ipv4 :host host :service *sb-port*))
+  (add-hook 'after-change-functions 'sb-send-update nil 'local)
+  (equal 'open (process-status *sb-server*)))
 
-(defun sb-update (start end bytes)
+(defun sb-connect-to-shared-buffer (host &optional buffer)
+  "This interactive function is used to connect to an already existing
+shared-buffer-session."
+  (interactive "sHost: ")
+  (if (sb-connect-to-server
+       host (or buffer (generate-new-buffer "*shared-buffer*")))
+      (process-send-string *sb-server* (concat "existing\n" *sb-key* "\n"))
+    (message "Could not connect."))
+  (set-process-filter *sb-server* 'sb-client-new-connection-filter))
+
+(defun sb-share-this-buffer (host &optional buffer)
+  "This interactive function is used to initiate a shared-buffer-session."
+  (interactive "sHost: ")
+  (if (sb-connect-to-server
+       host (or buffer (current-buffer)))
+      (process-send-string *sb-server* (concat "new\n" *sb-key* "\n"))
+    (message "Could not connect.")))
+
+(defun sb-send-update (start end bytes)
   "Sends an update to the server. This function is locally added
 to the 'after-change-functions hook for shared buffers."
   (let ((package
@@ -48,33 +74,62 @@ to the 'after-change-functions hook for shared buffers."
   (interactive)
   (delete-process *sb-server*)
   (setq *sb-server* nil)
-  (remove-hook 'after-change-functions 'sb-update 'local))
-
-(defun sb-possible-update-p (package)
-  (and (<= (+ (sb-package-start package)
-              (sb-package-bytes package))
-           (point-max))))
+  (remove-hook 'after-change-functions 'sb-send-update 'local))
 
 (defun sb-insert (strings)
+  "Each string in strings represents a line. Messages containing only one
+string does not contain a line break, but all others naturally does."
   (insert (car strings))
   (mapc (lambda (str)
-          (insert str) (newline)) (cdr strings)))
+          (newline) (insert str)) (cdr strings)))
 
-(defun sb-client-filter (process msg)
-  "The filter function handles all messages from the server."
-  (message msg)
+(defun message-from-server (process msg)
+  "The sever does not send messages with the sb-package format. These
+messages are handled in this function."
+  (cond ((string= msg "send-everything")
+         (process-send-string
+          *sb-server*
+          (concat (prin1-to-string
+                   (make-sb-package
+                    :text (s-lines (buffer-substring-no-properties
+                                    (point-min) (point-max)))
+                    :for-new-client t)) "\n")))
+        ((string-match "The key " msg)
+         (kill-buffer (process-buffer process))
+         (message msg))
+        (t (message msg))))
+
+(defun sb-update-buffer (package buffer)
+  "Makes changes to the shared buffer specified by the package."
   (save-excursion
-    (let ((buffer (current-buffer))
-          (package (read msg)))
-      (set-buffer (process-buffer process))
-      (remove-hook 'after-change-functions 'sb-update 'local)
-      (if (not (sb-possible-update-p package))
-          (message "sb-warning: out of sync! Run command: sb-sync")
+    (let ((old-curr-buf (current-buffer)))
+      (set-buffer buffer)
+      (remove-hook 'after-change-functions 'sb-send-update 'local)
+      (unless (sb-package-for-new-client package)
         (goto-char (sb-package-start package))
         (delete-char (sb-package-bytes package))
         (sb-insert (sb-package-text package)))
-      (add-hook 'after-change-functions 'sb-update nil 'local)
-      (set-buffer buffer))))
+      (add-hook 'after-change-functions 'sb-send-update nil 'local)
+      (set-buffer old-curr-buf))))
+
+(defun sb-client-new-connection-filter (process msg)
+  "This filter is used when a connection is initiated. During
+  most of a shared-buffer-session sb-client-filter will be used."
+  (save-excursion
+    (let ((old-curr-buf (current-buffer))
+          (package (read msg)))
+      (set-buffer (process-buffer process))
+      (remove-hook 'after-change-functions 'sb-send-update 'local)
+      (sb-insert (sb-package-text package))
+      (add-hook 'after-change-functions 'sb-send-update nil 'local)
+      (set-buffer old-curr-buf)))
+  (set-process-filter process 'sb-client-filter))
+
+(defun sb-client-filter (process msg)
+  "The filter function handles all messages from the server."
+  (if (not (string-match "\\[cl-struct-sb-package" msg))
+      (message-from-server process msg)
+    (sb-update-buffer (read msg) (process-buffer process))))
 
 (defun sb-client-sentinel (process msg)
   (message msg))
