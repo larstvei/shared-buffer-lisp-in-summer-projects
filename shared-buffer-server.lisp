81b8e322d90b056f3e71666fc8a9c243feb74980
Lars Tveito <larstvei@student.matnat.uio.no>
2013-08-04 19:43:05 +0200

    Restructured by adding structs and cleaning up code.
    Also added color, to be able to seperate the clients.

--- shared-buffer-server.lisp
+++ shared-buffer-server.lisp
@@ -16,35 +16,84 @@
 (defparameter *client-groups* nil
   "Hash table containing lists of clients working on the same buffer.")
 
+(defparameter *colors* (list "green" "blue" "red" "yellow" "purple" "orange")
+  "A list of colors for cursors.")
+
+(defstruct client key stream id color)
+
+(defstruct client-group clients colors)
+
 (defun send-package (message client-group)
   "Simply sends a message recived from a client to all clients sharing a
 buffer."
-  (loop for stream in client-group do
-       (write-string message stream)
-       (force-output stream)))
 
-(defun stream-reader (stream key)
-  ;; Reading the stream until EOF.
-  (loop for message = (read-line stream nil)
+  ;; -- DEBUG -- ;;
+  (print message)
+  (force-output)
+  ;; ----------- ;;
+
+  (loop for client in client-group do
+       (write-string message (client-stream client))
+       (force-output (client-stream client))))
+
+(defun remove-from-group (client)
+  "Fetches the client-group the given client is a part of, and returns it's
+list of clients without the given client."
+  (remove client (client-group-clients
+                  (gethash (client-key client) *client-groups*))))
+
+(defun stream-reader (client)
+  "After a client has successfully connected to the server, this function
+will be called. Every time it receives a package it will make a few changes
+  to the package and send it on to all the other clients sharing the same
+  buffer. "
+  (loop for message = (read-line (client-stream client) nil)
      while message do
-     ;; -- DEBUG -- ;;
-       (print
-        (concatenate 'string
-                     (subseq message 0 (- (length message) 4))
-                     (write-to-string (sxhash (write-to-string stream))) "]"))
-       (force-output)
-     ;; ----------- ;;
        (send-package
-        (concatenate 'string
-                     ;; Look at the whole message exept "nil]"
-                     (subseq message 0 (- (length message) 4))
-                     (write-to-string
-                      (sxhash (write-to-string stream))) "]")
-        (remove stream (gethash key *client-groups*)))
+        (format nil "~a ~a \"~a\"]"
+                (subseq message 0 (- (length message) 9))
+                (client-id client) (client-color client))
+        (remove-from-group client))
        (sleep 0.025))
-  ;; After reaching EOF we remove the client from the client group.
-  (setf (gethash key *client-groups*)
-        (remove stream (gethash key *client-groups*))))
+  ;; After reaching EOF we remove the client from the client group. If that
+  ;; was the last connected client the key should no longer be associated
+  ;; with a key. 
+  (unless (setf (client-group-clients
+                 (gethash (client-key client) *client-groups*))
+                (remove-from-group client))
+    (remhash (client-key client) *client-groups*)))
+
+(defun next-color (colors)
+  "Receives a list of colors, and returns two values: the first color and
+the same list of colors only rotated once to the left."
+  (values (car colors)
+          (append (cdr colors) (list (car colors)))))
+
+(defun connect-client-to-existing-session (stream key)
+  "This function is called if a client wants to be connected to an already
+existing shared buffer session. Here it's given an id, and a color."
+  (multiple-value-bind (color colors)
+      (next-color (client-group-colors (gethash key *client-groups*)))
+    (let ((client-group (gethash key *client-groups*))
+          (client (make-client
+                   :key key :stream stream :id (gensym) :color color)))
+      (send-package "send-everything" (client-group-clients client-group))
+      (setf (client-group-clients client-group)
+            (cons client (client-group-clients client-group)))
+      (setf (client-group-colors client-group) colors)
+      (stream-reader client))))
+
+(defun connect-client-to-new-session (stream key)
+  "This function is called when a client wants to create a new shared buffer
+session. We let the key be associated with a new client-group, which
+  contains the new client and a list of colors."
+  (multiple-value-bind (color colors)
+      (next-color *colors*)
+    (let ((client (make-client
+                   :key key :stream stream :id (gensym) :color color)))
+      (setf (gethash key *client-groups*)
+            (make-client-group :clients (list client) :colors colors))
+      (stream-reader client))))
 
 (defun handler (stream)
   "When a connection to a client is established this function is run. It
@@ -52,23 +101,19 @@ buffer."
   messages to all clients that has provided the same key."
   (let* ((kind (read-line stream))
          (key (read-line stream)))
-    (cond ((and (string= kind "new")
-                (gethash key *client-groups*))
-           (send-package "Choose a different key." (list stream)))
-          ((and (string= kind "existing")
-                (not (gethash key *client-groups*)))
-           (send-package
-            (concatenate 'string
-                         "The key " key " is not associated with any"
-                         " shared-buffer-session.") (list stream)))
-          ((or (string= kind "new")
-               (string= kind "existing"))
-           (setf (gethash key *client-groups*)
-                 (cons stream (gethash key *client-groups*)))
-           (when (string= kind "existing")
-             (send-package "send-everything"
-                           (list (second (gethash key *client-groups*)))))
-           (stream-reader stream key))
+    (cond ((string= kind "new")
+           (if (gethash key *client-groups*)
+               (send-package "Choose a different key."
+                             (list (make-client :stream stream)))
+               (connect-client-to-new-session stream key)))
+          ((string= kind "existing")
+           (if (gethash key *client-groups*)
+               (connect-client-to-existing-session stream key)
+               (send-package
+                (concatenate 'string
+                             "The key " key " is not associated with any"
+                             " shared buffer session.")
+                (list (make-client :stream stream)))))
           (t (send-package "Error in format." (list stream))))))
 
 (defun shared-buffer-server (host)
